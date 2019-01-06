3d9bd3fd4bce73240ac8f63ff2a0a6a250b072f1
이재준 <asapz@naver.com>
2019-01-06 17:38:10 +0900

    Created Home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 
 const App=()=>(
   <div>
@@ -6,10 +7,24 @@ const App=()=>(
   </div>
 );
 
+//welcome으로 이동시 화면나옴. exact는 안붙이면 /뒤에 potato가 오든 뭐가오든 경로인줄알고 찾아줌 확실한 경로 찾을때 exact
 const Header=()=>(
   <header>
     <h1>My Contents</h1>
+    <Route exact path="/" component={Welcome}/>  
+    <Route path="/contacts" component={Contacts}/>
   </header>
 );
 
+const Welcome=()=>(
+  <h1>Welcome to the contacts app!</h1>
+)
+
+const Contacts=()=>(
+  <ul>
+    <li>Lynn</li>
+    <li>Jisu</li>
+    <li>Japan Guy</li>
+  </ul>
+)
 export default App;
